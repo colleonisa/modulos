7af3a054ed1ed64b7088ca04ac98864e91af157e
mateus <mateusteti2007@gmail.com>
2023-08-29 15:19:13 -0300

    calculos

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,8 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
-
+var divisao = require("./modulos/divisao");
+var multi = require("./modulos/multi");
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multi(20,90));
+console.log(divisao(20,90));
